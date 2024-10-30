d7116a859507aece13dde1056d7dff5a7582e340
Kmerck88 <kevin.merck88@gmail.com>
2024-10-30 18:50:20 -0400

    Add NUnit test project for StudentGrades
    A new project `StudentGrades.nUnitTest` has been added to the Visual Studio solution `StudentGrades.sln` for unit testing using NUnit. This project is configured to build and run in both Debug and Release configurations.
    
    A new file `StudentCalculatorTests.cs` has been added, containing a test class `StudentCalculatorTests` with a setup method to initialize a `GradeCalculator` instance and a test method `GetGradeByPercentage_EqualTest` to verify the `GetGradeByPercentage` method returns "A" for various input percentages.
    
    The `StudentGrades.nUnitTest.csproj` file specifies the project as a .NET 8.0 test project and includes necessary package references for NUnit, NUnit analyzers, NUnit3TestAdapter, and coverlet.collector for code coverage. It also includes a project reference to the main `StudentGrades` project and a using directive for `NUnit.Framework`.

---
+++ StudentGrades.nUnitTest/StudentCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentGrades.nUnitTest
+{
+	public class StudentCalculatorTests
+	{
+		private GradeCalculator _gradeCalculator { get; set; } = null;
+		[SetUp]
+		public void Setup()
+		{
+			_gradeCalculator = new GradeCalculator();
+		}
+
+		[TestCase(91)]
+		[TestCase(95)]
+		[TestCase(97)]
+		[TestCase(99)]
+		
+		
+		public void GetGradeByPercentage_EqualTest(int percentage)
+		{
+
+			// assign
+			// var percentage = 99;
+
+			// act
+			var grade = _gradeCalculator.GetGradeByPercentage(percentage);
+
+			// assert
+			Assert.That(grade, Is.EqualTo("A"));
+		}
+	}
+}
