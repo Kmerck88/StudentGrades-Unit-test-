db819d80f7b4c5e5abf85ac86f597d7ab53611c9
Kmerck88 <kevin.merck88@gmail.com>
2024-10-30 18:07:31 -0400

    Add project files.

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentGrades
+{
+	public class Program
+	{
+		public string GetGradeByPercent(int percent)
+		{
+			if (percent >= 90 & percent <= 100)
+			
+				return "A";
+			
+			else if (percent >= 80 && percent < 90)
+			
+				return "B";
+			
+			else if (percent >= 70 && percent < 80)
+			
+				return "C";
+			
+			else if (percent >= 60 && percent < 70)
+			
+				return "D";
+			
+			else if (percent >= 0 && percent < 60)
+				return "F";
+			else
+					return "Invalid";
+			}
+		
+
+		}
+	}
